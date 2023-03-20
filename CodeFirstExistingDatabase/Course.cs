cb6f5544c92005afa76b7dd0d5eb1f75b29ee12a
Nikos Karampas <nikos.karampas@hotmail.com>
2023-03-20 17:17:44 +0200

    Delete Categories table and preserve its data in _Categories table

--- CodeFirstExistingDatabase/Course.cs
+++ CodeFirstExistingDatabase/Course.cs
@@ -19,9 +19,7 @@ namespace CodeFirstExistingDatabase
 
         public string Name { get; set; }
 
-        public string Description { get; set; }
-
-        public Category Category { get; set; }       
+        public string Description { get; set; }               
 
         public int Level { get; set; }
 
--- CodeFirstExistingDatabase/PlutoContext.cs
+++ CodeFirstExistingDatabase/PlutoContext.cs
@@ -14,8 +14,7 @@ namespace CodeFirstExistingDatabase
 
         public virtual DbSet<Author> Authors { get; set; }
         public virtual DbSet<Course> Courses { get; set; }
-        public virtual DbSet<Tag> Tags { get; set; }
-        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }        
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
