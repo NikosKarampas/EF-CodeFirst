5cb01769a7d41738e813bcc7887e9ea03d47199e
Nikos Karampas <nikos.karampas@hotmail.com>
2023-03-21 15:52:31 +0200

    LINQ queries using LINQ syntax and extension methods

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -10,6 +10,26 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            // LINQ syntaxt
+            var query = from c in context.Courses
+                        where c.Name.Contains("c#")
+                        orderby c.Name
+                        select c;            
+
+            //foreach (var c in query)
+            //    Console.WriteLine(c.Name);
+
+            // Extension Methods
+            var courses = context.Courses
+                .Where(c => c.Name.Contains("c#"))
+                .OrderBy(c => c.Name);
+
+            foreach (var c in courses)
+                Console.WriteLine(c.Name);
+            
+            Console.ReadKey();
         }
     }
 }
