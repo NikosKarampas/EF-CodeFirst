e8bf9a1fc292a40429cd59c6cccd4d1c979072a9
Nikos Karampas <nikos.karampas@hotmail.com>
2023-03-21 16:26:26 +0200

    Order by/Group by, joins LINQ syntax/extension methods

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -26,9 +26,63 @@ namespace Queries
                 .Where(c => c.Name.Contains("c#"))
                 .OrderBy(c => c.Name);
 
-            foreach (var c in courses)
-                Console.WriteLine(c.Name);
-            
+            //foreach (var c in courses)
+            //    Console.WriteLine(c.Name);
+
+            #region Order by/Group by
+
+            var query1 = from c in context.Courses
+                         where c.Author.Id == 1
+                         orderby c.Level descending, c.Name
+                         select new { Name = c.Name, Author = c.Author.Name };
+
+            var query2 = from c in context.Courses
+                         group c by c.Level 
+                         into g
+                         select g;
+
+            //foreach (var group in query2)
+            //{
+            //    Console.WriteLine(group.Key);
+
+            //    foreach (var course in group)
+            //    {
+            //        Console.WriteLine("\t{0}", course.Name);
+            //    }
+            //}
+
+            //foreach (var group in query2)
+            //{
+            //    Console.WriteLine("{0} ({1})", group.Key, group.Count());
+            //}
+
+            #endregion
+
+
+            #region Joins LINQ syntax/extension methods
+
+            var query3 = from c in context.Courses
+                         join a in context.Authors
+                         on c.AuthorId equals a.Id
+                         select new { CourseName = c.Name, AuthorName = a.Name };
+
+            var query4 = context.Courses.Join(context.Authors, 
+                c => c.AuthorId, 
+                a => a.Id, 
+                (course, author) => 
+                    new 
+                    { 
+                        CourseName = course.Name,
+                        AuthorName = author.Name 
+                    });
+
+            foreach ( var c in query4)
+            {
+                Console.WriteLine(c.CourseName + " " + c.AuthorName);
+            }
+
+            #endregion
+
             Console.ReadKey();
         }
     }
