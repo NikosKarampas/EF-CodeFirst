8aa3de0724d3676acb5fead221e07c8e71147752
Nikos Karampas <nikos.karampas@hotmail.com>
2023-03-23 12:59:52 +0200

    Add a new object (course) to the database

--- UpdatingData/Program.cs
+++ UpdatingData/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Queries;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,24 @@ namespace UpdatingData
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            //var authors = context.Authors.ToList();
+            //var author = authors.Single(a => a.Id == 1);
+
+            var course = new Course
+            {
+                Name = "New Course",
+                Description = "New Description",
+                FullPrice = 19.95f,
+                Level = 1,
+                AuthorId = 1
+                //Author = author
+            };
+
+            context.Courses.Add(course);
+
+            context.SaveChanges();
         }
     }
 }
